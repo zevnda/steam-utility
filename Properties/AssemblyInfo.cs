fa880bdb74732215d43889bc39acec26c8ed4cbd
ProbablyRaging <probablyraging@gmail.com>
2025-02-04 08:38:53 +1100

    Update copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Utility")]
-[assembly: AssemblyCopyright("Copyright © 2024 zevnda")]
+[assembly: AssemblyCopyright("Copyright © 2025 zevnda")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
